a2e80f10eb7f4f214b28a91e7a9b8f5e1a175173
dragon-fish <dragon-fish@qq.com>
2026-04-14 12:06:53 +0800

    build: add preact, preact-iso, signals, unocss dependencies and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import preact from '@preact/preset-vite'
+import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
   base: './',
+  plugins: [preact(), UnoCSS()],
   resolve: {
     alias: {
       '@': resolve(import.meta.dirname, 'src'),
